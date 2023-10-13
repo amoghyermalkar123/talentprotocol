1d0f45dd897ee2f643ed27889cf397ba29e3f68a
amoghyermalkar123 <ayermalkar99@gmail.com>
2023-10-13 10:19:47 +0530

    commit

--- types/org.go
+++ types/org.go
@@ -22,6 +22,7 @@ type Organization struct {
 type JobOpening struct {
 	ID             string    `bson:"_id,omitempty" json:"_id"`
 	OrganizationID string    `bson:"org_id" json:"org_id"`
+	OrgName        string    `bson:"org_name" json:"org_name"`
 	OpeningName    string    `bson:"opening_name" json:"opening_name"`
 	JobDescription string    `bson:"jd" json:"jd"`
 	JobPostedAt    time.Time `bson:"job_posted_at" json:"job_posted_at,omitempty"`
